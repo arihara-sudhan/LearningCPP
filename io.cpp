c8736510c503b4aca65f9f345949387eb93efaac
Aravind Ariharasudhan <100225369+arihara-sudhan@users.noreply.github.com>
2023-08-13 08:34:37 +0530

    Add files via upload

---
+++ io.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int num1;
+	float num2;
+	char name[10];
+	cin >> num1;
+	cin >> num2;
+	cin >> name;
+	cout << "\nYou Entered INTEGER : "<< num1;
+	cout << "\nYou Entered FLOAT : "<< num2;
+	cout << "\nYou Entered STRING : "<< name;
+	return 0;
+}
