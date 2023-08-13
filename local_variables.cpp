50fd0ac95014525a058682072b8720e6227ae23a
Aravind Ariharasudhan <100225369+arihara-sudhan@users.noreply.github.com>
2023-08-13 08:39:53 +0530

    Add files via upload

---
+++ local_variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	// DECLARE WHEREVER WE WANT UNLIKE IN C
+	int num1 = 2;
+	cout <<"\n"<<num1;
+	float num2 = 3.2;
+	cout <<"\n"<<num2;
+	char name[10] = "ARIHARAN";
+	cout <<"\n"<< name;
+	return 0;
+}
